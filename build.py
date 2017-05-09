de8776b880b8541536918cfafe519d86b9c27061
Luis <lasote@gmail.com>
2017-05-09 10:53:40 +0200

    Update build.py
    Fixed CI

--- build.py
+++ build.py
@@ -5,13 +5,13 @@ if __name__ == "__main__":
     builder = ConanMultiPackager()
     builder.add_common_builds(shared_option_name="gtest:shared", pure_c=False)
     filtered_builds = []
-    for settings, options in builder.builds:
+    for settings, options, env_vars, build_requires in builder.builds:
         if settings["compiler"] == "Visual Studio":
             pdbOptions = options.copy()
             pdbOptions.update({"gtest:include_pdbs": "True"})
-            filtered_builds.append([settings, pdbOptions])
+            filtered_builds.append([settings, pdbOptions, env_vars, build_requires])
         
-        filtered_builds.append([settings, options])
+        filtered_builds.append([settings, options, env_vars, build_requires])
 
     builder.builds = filtered_builds
     builder.run()
